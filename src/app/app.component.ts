230c7742a308f8f04374a028b5a195a9cdeb67c9
uibhanu5 <ui.bhanu5@gmail.com>
2022-09-13 21:56:03 +0530

    code refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -218,64 +218,14 @@ export class AppComponent {
     },
   ];
 
-  // checkMinusSquare(item) {
-  //   const count = item.childs.filter(x => x.checked == true).length;
-  //   if (count > 0 && count < item.childs.length) {
-  //     return true;
-  //   } else if (count == 0) {
-  //     return false;
-  //   }
-  // }
-
-  // checkParent(group_i, i) {
-  //   this.arr[group_i].items[i].checked = !this.arr[group_i].items[i].checked;
-  //   if (this.arr[group_i].items[i].checked) {
-  //     this.arr[group_i].items[i].childs.map((x) => (x.checked = true));
-  //   } else {
-  //     this.arr[group_i].items[i].childs.map((x) => (x.checked = false));
-  //   }
-  //   this.arr[group_i].items[i].childs.forEach((x) => {
-  //     if (this.listChildChanged.findIndex((el) => el.id == x.id) == -1) {
-  //       this.listChildChanged.push(x);
-  //     }
-  //   });
-  // }
 
   checkChild(group_i, parent_i, i) {
     this.arr[group_i].items[parent_i].childs[i].expand =
       !this.arr[group_i].items[parent_i].childs[i].expand;
-    // const count = this.arr[group_i].items[parent_i].childs.filter(
-    //   (el) => el.expand == true
-    // ).length;
-    // if (count == this.arr[group_i].items[parent_i].childs.length) {
-    //   this.arr[group_i].items[parent_i].expand = true;
-    // } else {
-    //   this.arr[group_i].items[parent_i].expand = false;
-    // }
-    // if (
-    //   this.listChildChanged.findIndex(
-    //     (el) => el.id == this.arr[group_i].items[parent_i].childs[i].id
-    //   ) == -1
-    // ) {
-    //   this.listChildChanged.push(this.arr[group_i].items[parent_i].childs[i]);
-    // }
   }
 
   checkSubChild(group_i, parent_i, child_i, i) {
     this.arr[group_i].items[parent_i].childs[child_i].subChilds[i].expand =
       !this.arr[group_i].items[parent_i].childs[child_i].subChilds[i].expand;
-    // const count = this.arr[group_i].items[parent_i].childs[child_i].subChilds.filter(
-    //   (el) => el.expand == true
-    // ).length;
-    // if (count == this.arr[group_i].items[parent_i].childs[child_i].subChilds.length) {
-    //   this.arr[group_i].items[parent_i].childs[child_i].expand = true;
-    // } else {
-    //   this.arr[group_i].items[parent_i].childs[child_i].expand = false;
-    // }
-
   }
-
-  // getListChildChanged() {
-  //   console.log(this.listChildChanged);
-  // }
 }
